cf2ffa5e41e0a4e85f3b205401b91bc28592fcc4
D3bu9m4n <51529146+D3bu9m4n@users.noreply.github.com>
2025-01-24 17:40:38 +0800

    Update xhs_like.js

--- xhs_like.js
+++ xhs_like.js
@@ -5,8 +5,8 @@ let obj = JSON.parse(body);
 if (obj.data && obj.data.notes && obj.data.notes.length > 0) {
     // 生成1000-1800之间的随机数
     // const randomLikes = Math.floor(Math.random() * (1800 - 1000 + 1)) + 1000;
-    const randomLikes = 1399;
-    const randomLikes2 = 1487;
+    const randomLikes = 1755;
+    const randomLikes2 = 1879;
     // 修改第一条笔记的点赞数
     obj.data.notes[0].badge_info.show_content = randomLikes;
     obj.data.notes[1].badge_info.show_content = randomLikes2;
